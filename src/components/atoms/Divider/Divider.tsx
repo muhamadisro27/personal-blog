7e015cac9eac88d5de6654e4f384511c5adc95f1
isro <mohammadisro2710@gmail.com>
2025-05-25 13:06:52 +0700

    style: change style divider

--- src/components/atoms/Divider/Divider.tsx
+++ src/components/atoms/Divider/Divider.tsx
@@ -4,7 +4,12 @@ import React, { ComponentPropsWithoutRef } from "react"
 type Props = ComponentPropsWithoutRef<"hr">
 
 const Divider = ({ className, ...props }: Props) => {
-  return <hr className={cn("mt-5", className)} {...props} />
+  return (
+    <hr
+      className={cn("mt-[1rem] h-[1px] block border border-t-[#404040] mx-auto", className)}
+      {...props}
+    />
+  )
 }
 
 export default Divider
